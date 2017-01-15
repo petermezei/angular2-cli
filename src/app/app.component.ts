3e49a3a8142078f06c0b267f46ad886c811029d6
Peter Mezei <peter.mezei@slamby.com>
2017-01-15 16:31:42 +0100

    sass test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { DemoService } from "./demo.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [DemoService]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private demoService: DemoService){}
   title = 'app works!';
-  msg: string;
-  hello() {
-    this.msg = this.demoService.sayHello("Peti");
+  hello(){
+    //
   }
 }
